b7f15d3aa0a8058c4f941b62cc5147e22ce72176
ykhs <yukihisa.yamada@gmail.com>
2016-10-27 01:20:39 +0900

    ボタン押してインストールするように

--- example/1-install/app.js
+++ example/1-install/app.js
@@ -1,19 +1,27 @@
 const name = '[Browser]';
 
-if ('serviceWorker' in navigator) {
-  console.log('%s Service Workerに対応しています。', name);
+function register() {
+  if ('serviceWorker' in navigator) {
+    console.log('%s Service Workerに対応しています。', name);
 
-  const options = {
-    scope: './'
-  };
+    const options = {
+      scope: './'
+    };
 
-  navigator.serviceWorker.register('./service-worker.js', options)
-    .then((registration) => {
-      console.log('%s Service Workerが登録されました。', name, registration);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-} else {
-  console.warn('%s Service Workerに対応していません。', name);
+    navigator.serviceWorker.register('./service-worker.js', options)
+      .then((registration) => {
+        console.log('%s Service Workerが登録されました。', name, registration);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    console.warn('%s Service Workerに対応していません。', name);
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('register').addEventListener('click', () => {
+    register();
+  });
+});
