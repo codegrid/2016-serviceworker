044a632dc35297c1dabcfa3990e15e688289f2c6
ykhs <yukihisa.yamada@gmail.com>
2016-10-28 16:09:11 +0900

    update examples

--- example/2-install-and-ready/app.js
+++ example/2-install-and-ready/app.js
@@ -11,6 +11,14 @@ function register() {
     navigator.serviceWorker.register('./service-worker.js', options)
       .then(registration => {
         console.log('%s Service Workerが登録されました。', name);
+        return navigator.serviceWorker.ready;
+      })
+      .then(registration => {
+        const serviceWorker = registration.active;
+        console.info('%s Service Workerは [%s] 状態です。', name, serviceWorker.state);
+        serviceWorker.addEventListener('statechange', (event) => {
+          console.info('%s Service Workerが [%s] 状態になりました。', name, event.target.state);
+        });
       })
       .catch(error => {
         console.error('%s Service Workerの登録に失敗しました。', name, error);
